132587942853bb2346b1d936e7441be11b1853b6
이유정 <yujlee242@gmail.com>
2022-07-27 20:32:54 +0900

    💄 버튼 컴포넌트 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Buttons from './Buttons';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Buttons category={'negative'} text={'삭제하기'} />
+      <Buttons category={'primary'} text={'오늘의 일기 작성하기'} />
+      <Buttons category={'secondary'} text={'삭제하기'} />
+      <Buttons text={'뒤로가기'} />
     </div>
   );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,5 +1,5 @@
 const Buttons = ({ category, text, onClick }) => {
-  const btnType = ['negative', 'positive'].includes(category) ? category : 'default';
+  const btnType = ['primary', 'secondary'].includes(category) ? category : 'default';
 
   return (
     <button type="button" className={['btn', `btn-${btnType}`].join(' ')} onClick={onClick}>
