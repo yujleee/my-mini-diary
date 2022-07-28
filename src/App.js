c6ba99f013f84ffb89bef8d6cd68352919674773
이유정 <yujlee242@gmail.com>
2022-07-28 17:50:08 +0900

    ✨  헤더 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <Buttons category={'primary'} text={'오늘의 일기 작성하기'} />
       <Buttons category={'secondary'} text={'삭제하기'} />
       <Buttons text={'뒤로가기'} />
+      <Header left={<Buttons text={'<'} />} titleText={'홈'} right={<Buttons text={'>'} />} />
+      <Header titleText={'일기 작성'} left={<Buttons text={'<'} />} />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = ({ left, titleText, right }) => {
+  return (
+    <header className="header">
+      <div className="left">{left}</div>
+      <h1 className="title">{titleText}</h1>
+      <div className="right">{right}</div>
+    </header>
+  );
+};
+
+export default Header;
