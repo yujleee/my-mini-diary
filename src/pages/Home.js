f9bf1bcb15d1bb3eba49dd467f7db5a495c43661
이유정 <yujlee242@gmail.com>
2022-07-29 23:27:22 +0900

    🚧  홈화면 헤더 컴포넌트 추가 좌우 버튼에 따라 월 단위로 상태변경될 수 있도록 함

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import Buttons from '../components/Buttons';
+import Header from '../components/Header';
+
 const Home = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const title = `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`;
+
+  const decreaseMonth = () =>
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate()));
+  const increaseMonth = () =>
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()));
+
   return (
     <div>
-      <h2>home</h2>
+      <Header
+        left={<Buttons text={'<'} onClick={decreaseMonth} />}
+        titleText={title}
+        right={<Buttons text={'>'} onClick={increaseMonth} />}
+      />
     </div>
   );
 };
