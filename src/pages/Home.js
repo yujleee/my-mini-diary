3c502c915387caed8c3c1038872cf53b73cdd516
이유정 <yujlee242@gmail.com>
2022-09-08 23:04:14 +0900

    ✨ 홈 컴포넌트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { DataStateContext } from '../App';
 import Buttons from '../components/Buttons';
 import Header from '../components/Header';
 import DiaryList from '../components/DiaryList';
 
+import '../style/home.css';
+
 const Home = () => {
+  const navigate = useNavigate();
   const diaryList = useContext(DataStateContext);
   const [currentDate, setCurrentDate] = useState(new Date());
   const title = `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`;
@@ -14,14 +18,19 @@ const Home = () => {
   const increaseMonth = () =>
     setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()));
 
+  const goNew = () => {
+    navigate('/new');
+  };
+
   return (
-    <div>
+    <div className="home">
       <Header
         left={<Buttons text={'<'} onClick={decreaseMonth} />}
         titleText={title}
         right={<Buttons text={'>'} onClick={increaseMonth} />}
       />
       <DiaryList diaryList={diaryList} />
+      <Buttons text={'새로운 일기 쓰기'} category={'primary'} onClick={goNew} />
     </div>
   );
 };
