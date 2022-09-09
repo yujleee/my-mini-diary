e9a0a1703908def4cd6b4c68b5bd0de2145e064b
이유정 <yujlee242@gmail.com>
2022-09-09 20:00:48 +0900

    ✨ 필터링 컴포넌트 추가

--- src/components/ControlMenu.js
+++ src/components/ControlMenu.js
@@ -1,6 +1,6 @@
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)} optionList={optionList}>
+    <select className="control-menu" value={value} onChange={(e) => onChange(e.target.value)}>
       {optionList.map((it, idx) => (
         <option key={idx} value={it.value}>
           {it.name}
